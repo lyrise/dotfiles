7991dfb6aa12dd73388c8ac2d1db3d29fcbeef71
Lyrise <lyrise1984@gmail.com>
2020-05-31 15:58:36 +0900

    fix: install_ubuntu.sh

--- install_ubuntu.sh
+++ install_ubuntu.sh
@@ -8,10 +8,10 @@ wget -q https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-pr
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 sudo add-apt-repository universe
-sudo apt update
-sudo apt install -y apt-transport-https
-sudo apt update
-sudo apt install -y dotnet-sdk-3.1
+sudo apt-get update
+sudo apt-get install -y apt-transport-https
+sudo apt-get update
+sudo apt-get install -y dotnet-sdk-3.1
 
 # rust
 curl https://sh.rustup.rs -sSf | sh -s -- -q -y
