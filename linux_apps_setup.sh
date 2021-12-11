9168f22fedd625573ae136e04342ca773b5ee4b7
Lyrise <lyrise1984@gmail.com>
2021-12-11 13:45:38 +0900

    fix

--- linux_apps_setup.sh
+++ linux_apps_setup.sh
@@ -20,7 +20,7 @@ git clone --depth 1 https://github.com/junegunn/fzf.git $HOME/.fzf
 ~/.fzf/install --no-key-bindings --no-completion --no-bash --no-zsh
 
 # install rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -q -y
 export PATH="$HOME/.cargo/bin:$PATH"
 
 # install tools
