97aba3ae5864d5b6e4ada8fa7b04daadfd2f7d72
Lyrise <lyrise1984@gmail.com>
2021-12-28 16:32:09 +0900

    add: jdk and sbt

--- linux_apps_setup.sh
+++ linux_apps_setup.sh
@@ -16,9 +16,24 @@ sudo apt-get install -y \
 # set default shell
 sudo chsh -s $(which zsh)
 
+# install zplug
+mkdir ~/.zinit
+git clone https://github.com/zdharma-continuum/zinit.git ~/.zinit/bin
+
 # install fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --no-key-bindings --no-completion --no-bash --no-zsh
+
+# Install jdk
+sudo apt-get install -y openjdk-11-jdk
+
+# Install sbt
+SBT_VERSION=1.6.0
+sudo curl -L -o sbt-$SBT_VERSION.deb https://repo.scala-sbt.org/scalasbt/debian/sbt-$SBT_VERSION.deb
+sudo dpkg -i sbt-$SBT_VERSION.deb
+sudo rm sbt-$SBT_VERSION.deb
+sudo apt-get update
+sudo apt-get install -y sbt
 
 # install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -q -y
@@ -30,7 +45,3 @@ git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 # install tools
 sudo apt-get install -y fd-find ripgrep
 cargo install cargo-edit bat lsd
-
-# install zplug
-mkdir ~/.zinit
-git clone https://github.com/zdharma-continuum/zinit.git ~/.zinit/bin
